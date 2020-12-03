3c6f4903d53cd243e2a4a8200c58cbbb8e379e6d
Thomas Stjernegaard <tsjeppesen@gbif.org>
2020-12-03 19:01:27 +0100

    Use button, not a tag

--- src/components/OTL/Otl.js
+++ src/components/OTL/Otl.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import {  AutoComplete, Row, Col, Input, Form, message } from 'antd';
-import { withRouter } from 'react-router-dom';
-import _ from 'lodash';
-import axios from 'axios'
+import React from "react";
+import { AutoComplete, Row, Col, Input, Form, Button, message } from "antd";
+import { withRouter } from "react-router-dom";
+import _ from "lodash";
+import axios from "axios";
 
 const { Option } = AutoComplete;
 const FormItem = Form.Item;
@@ -23,81 +23,117 @@ class Upload extends React.Component {
     this.state = { names: [], node_id: "", loading: false };
   }
 
-getData = async (value) => {
-    let response = await axios.get(`//api.gbif.org/v1/species/suggest?q=${value}&datasetKey=d7dddbf4-2cf0-4f39-9b2a-bb099caae36c`);
-    this.setState({names: response.data}) ;
-}
+  getData = async (value) => {
+    let response = await axios.get(
+      `//api.gbif.org/v1/species/suggest?q=${value}&datasetKey=d7dddbf4-2cf0-4f39-9b2a-bb099caae36c`
+    );
+    this.setState({ names: response.data });
+  };
 
-getDataFromOtl = async (canonicalName, obj) => {
-  const nubKey = _.get(obj, 'key')
-  this.setState({value: canonicalName})
-  try {
-    this.setState({loading: true})
-    
-   
-    const response = await axios.get(`//www.gbif.org/api/otl/ottid?canonicalName=${canonicalName}&nubKey=${nubKey}`);
-    message.info("Got match in Open Tree of Life, fetching Tree")
-    const treeResponse = await axios.get(`//www.gbif.org/api/otl/newick?ott_id=${_.get(response, 'data.ott_id')}`)
-    this.props.setTree(_.get(treeResponse, 'data.newick'))
-    this.setState({loading: false})
-  } catch(err){
-    console.log(err.response)
-    if(_.get(err, "response.status") === 404){
-      message.info(`Sorry no match in Open Tree of Life for ${canonicalName}`)
+  getDataFromOtl = async (canonicalName, obj) => {
+    const nubKey = _.get(obj, "key");
+    this.setState({ value: canonicalName });
+    try {
+      this.setState({ loading: true });
 
+      const response = await axios.get(
+        `//www.gbif.org/api/otl/ottid?canonicalName=${canonicalName}&nubKey=${nubKey}`
+      );
+      message.info("Got match in Open Tree of Life, fetching Tree");
+      const treeResponse = await axios.get(
+        `//www.gbif.org/api/otl/newick?ott_id=${_.get(response, "data.ott_id")}`
+      );
+      this.props.setTree(_.get(treeResponse, "data.newick"));
+      this.setState({ loading: false });
+    } catch (err) {
+      console.log(err.response);
+      if (_.get(err, "response.status") === 404) {
+        message.info(
+          `Sorry no match in Open Tree of Life for ${canonicalName}`
+        );
+      }
+      this.setState({ loading: false });
     }
-    this.setState({loading: false})
-  }
+  };
+  getTreeFromOtl = async (node_id) => {
+    try {
+      this.setState({ loading: true });
 
-}
-getTreeFromOtl = async (node_id) => {
-  try {
-    this.setState({loading: true})
-
-    const treeResponse = await axios.get(`//www.gbif.org/api/otl/newick?node_id=${node_id}`)
-    this.props.setTree(_.get(treeResponse, 'data.newick'))
-    this.setState({loading: false})
-  } catch(err){
-    this.setState({loading: false})
-  }
-
-}
+      const treeResponse = await axios.get(
+        `//www.gbif.org/api/otl/newick?node_id=${node_id}`
+      );
+      this.props.setTree(_.get(treeResponse, "data.newick"));
+      this.setState({ loading: false });
+    } catch (err) {
+      this.setState({ loading: false });
+    }
+  };
   render() {
-      const {names, loading, node_id} = this.state;
+    const { names, loading, node_id } = this.state;
     return (
-    <React.Fragment>
-      <FormItem
-        {...formItemLayout}
-        help="Search for a Taxon in GBIF - we will try to get a Tree from Open Tree of Life"
-      ><AutoComplete 
-      onSearch={this.getData}
-      onSelect={this.getDataFromOtl}
-      style={{width: "100%"}}
-      loading={loading}
-      allowClear
-      placeholder="Type Taxon name"
-      >
-          {names.map(n => <Option key={n.key} value={n.canonicalName}>{n.canonicalName}</Option>)}
-      </AutoComplete></FormItem>
-      <Row><Col flex="auto"></Col><Col><h3>OR</h3></Col><Col flex="auto"></Col></Row>
-      <FormItem
-        {...formItemLayout}
-        help={<span>Or try this example: <a href="/#" onClick={()=> this.setState({node_id: "mrcaott71634ott117981"}) }>mrcaott71634ott117981</a></span>}
-      >
-        <Input.Search 
+      <React.Fragment>
+        <FormItem
+          {...formItemLayout}
+          help="Search for a Taxon in GBIF - we will try to get a Tree from Open Tree of Life"
+        >
+          <AutoComplete
+            onSearch={this.getData}
+            onSelect={this.getDataFromOtl}
+            style={{ width: "100%" }}
             loading={loading}
-            placeholder="Enter a OTL node_id like mrcaott71634ott117981" 
+            allowClear
+            placeholder="Type Taxon name"
+          >
+            {names.map((n) => (
+              <Option key={n.key} value={n.canonicalName}>
+                {n.canonicalName}
+              </Option>
+            ))}
+          </AutoComplete>
+        </FormItem>
+        <Row>
+          <Col flex="auto"></Col>
+          <Col>
+            <h3>OR</h3>
+          </Col>
+          <Col flex="auto"></Col>
+        </Row>
+        <FormItem
+          {...formItemLayout}
+          help={
+            <span>
+              Or try this example:{" "}
+              <Button
+                type="link"
+                onClick={() =>
+                  this.setState({ node_id: "mrcaott71634ott117981" })
+                }
+              >
+                mrcaott71634ott117981
+              </Button>
+            </span>
+          }
+        >
+          <Input.Search
+            loading={loading}
+            placeholder="Enter a OTL node_id like mrcaott71634ott117981"
             value={node_id}
-            onChange={e => {
-            this.setState({node_id: e.currentTarget.value})}}
+            onChange={(e) => {
+              this.setState({ node_id: e.currentTarget.value });
+            }}
             onSearch={this.getTreeFromOtl}
-            allowClear/>
-      </FormItem>
-      <Row><Col flex="auto"></Col><Col><h3>OR</h3></Col><Col flex="auto"></Col></Row>
-
-    </React.Fragment>
-      
-    )
+            allowClear
+          />
+        </FormItem>
+        <Row>
+          <Col flex="auto"></Col>
+          <Col>
+            <h3>OR</h3>
+          </Col>
+          <Col flex="auto"></Col>
+        </Row>
+      </React.Fragment>
+    );
   }
 }
 
