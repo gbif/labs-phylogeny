662dbee97754111adc1daec4058350e19a363560
Morten Høfft <mhoefft@gbif.org>
2021-11-01 10:58:12 +0100

    store state in localhost between sessions

--- src/components/ContextProvider.js
+++ src/components/ContextProvider.js
@@ -2,24 +2,47 @@ import React from "react";
 export const AppContext = React.createContext({});
 
 class ContextProvider extends React.Component {
-  state = {
-    newick: null,
-    rawTree: null,
-    names: null,
-    matchedNames: null,
-    setNewick: (newick) => {
-      this.setState({ newick });
-    },
-    setRawTree: (rawTree) => {
-      this.setState({ rawTree });
-    },
-    setNames: (names) => {
-      this.setState({ names });
-    },
-    setMatchedNames: (matchedNames) => {
-      this.setState({ matchedNames });
+  constructor(props) {
+    super(props);
+    this.state = {
+      newick: localStorage.getItem('newick') || null,
+      rawTree: this.getJSONFromStorage('rawTree'),
+      names: this.getJSONFromStorage('names'),
+      matchedNames: this.getJSONFromStorage('matchedNames'),
+      setNewick: (newick) => {
+        this.setState({ newick });
+        localStorage.setItem('newick', newick);
+        console.log('newick ', typeof newick);
+      },
+      setRawTree: (rawTree) => {
+        this.setState({ rawTree });
+        localStorage.setItem('rawTree', JSON.stringify(rawTree));
+        console.log('rawTree ', typeof rawTree);
+      },
+      setNames: (names) => {
+        this.setState({ names });
+        localStorage.setItem('names', JSON.stringify(names));
+        console.log('names ', typeof names);
+      },
+      setMatchedNames: (matchedNames) => {
+        this.setState({ matchedNames });
+        localStorage.setItem('matchedNames', JSON.stringify(matchedNames));
+        console.log('matchedNames ', typeof matchedNames);
+      }
+    };
+    console.log(this.state);
+  }
+
+  getJSONFromStorage = function(name) {
+    const nameStr = localStorage.getItem(name);
+    try {
+      const parsed = nameStr ? JSON.parse(nameStr) : null
+      return parsed;
+    } catch (err) {
+      console.error('invalid JSON loaded from storage');
+      return null;
     }
-  };
+  }
 
   render() {
     return (
