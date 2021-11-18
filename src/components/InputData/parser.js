381f6cce87396e031aa525a9b1164a7dd704563e
Morten Høfft <mhoefft@gbif.org>
2021-11-18 21:54:40 +0100

    linting

--- src/components/InputData/parser.js
+++ src/components/InputData/parser.js
@@ -42,39 +42,38 @@ function getTokens(s) {
 }
 
 function parser(s) {
-	var ancestors = [];
-	var tree = {};
-	// var tokens = s.split(/\s*(;|\(|\)|,|:)\s*/);
-	var tokens = getTokens(s);
-	for (var i=0; i<tokens.length; i++) {
-		var token = tokens[i];
-		switch (token) {
-			case '(': // new children
-				var subtree = {};
-				tree.children = [subtree];
-				ancestors.push(tree);
-				tree = subtree;
-				break;
-			case ',': // another branch
-				var subtree = {};
-				ancestors[ancestors.length-1].children.push(subtree);
-				tree = subtree;
-				break;
-			case ')': // optional name next
-				tree = ancestors.pop();
-				break;
-			case ':': // optional length next
-				break;
-			default:
-				var x = tokens[i-1];
-				if (x == ')' || x == '(' || x == ',') {
-					tree.name = token;
-				} else if (x == ':') {
-					tree.branch_length = parseFloat(token);
-				}
-		}
-	}
-	return tree;
+  var ancestors = [];
+  var tree = {};
+  // var tokens = s.split(/\s*(;|\(|\)|,|:)\s*/);
+  var tokens = getTokens(s);
+  for (var i = 0; i < tokens.length; i++) {
+    var token = tokens[i];
+    var subtree = {};
+    switch (token) {
+      case '(': // new children
+        tree.children = [subtree];
+        ancestors.push(tree);
+        tree = subtree;
+        break;
+      case ',': // another branch
+        ancestors[ancestors.length - 1].children.push(subtree);
+        tree = subtree;
+        break;
+      case ')': // optional name next
+        tree = ancestors.pop();
+        break;
+      case ':': // optional length next
+        break;
+      default:
+        var x = tokens[i - 1];
+        if (x === ')' || x === '(' || x === ',') {
+          tree.name = token;
+        } else if (x === ':') {
+          tree.branch_length = parseFloat(token);
+        }
+    }
+  }
+  return tree;
 };
 
 // const str = '(\'Bryophyta:123\':1, Chlorophyta something:2, "name two (author) 1970:63")Plantae;';
