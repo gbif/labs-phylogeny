6adce0b12f323be4f5327b807038eecce116fa91
Morten Høfft <6759267+MortenHofft@users.noreply.github.com>
2023-03-08 11:28:09 +0100

    fix: error handling and url encoding

--- src/components/Match/Match.js
+++ src/components/Match/Match.js
@@ -1,4 +1,4 @@
-import React, {useEffect, useState} from 'react';
+import React, { useEffect, useState } from 'react';
 import { Button, Typography, Card, Alert, Table, Progress } from 'antd';
 import { withRouter } from 'react-router-dom';
 import async from 'async';
@@ -78,14 +78,22 @@ class MatchNames extends React.Component {
   }
 
   lookupName = async (name, callback) => {
-    let response = await axios.get(`//api.gbif.org/v1/species/match?verbose=true&name=${name.name.replace(/_/g, ' ')}`);
-    this.setState({progress: this.state.progress + 1})
-    name.match = response.data;
-    if (name.match.rank === "UNRANKED") {
-      let formattedResponse = await axios.get(`https://www.gbif.org/api/species/${name.match.usageKey}/name`);
-      name.match.formattedName = _.get(formattedResponse, "data.n");
+    try {
+      let response = await axios.get(`//api.gbif.org/v1/species/match?verbose=true&name=${encodeURIComponent(name.name.replace(/_/g, ' '))}`);
+      this.setState({ progress: this.state.progress + 1 })
+      name.match = response.data;
+      if (name.match.rank === "UNRANKED") {
+        let formattedResponse = await axios.get(`https://www.gbif.org/api/species/${name.match.usageKey}/name`);
+        name.match.formattedName = _.get(formattedResponse, "data.n");
+      }
+      callback();
+    } catch (e) {
+      console.error(e);
+      // temporary fix for GBIF API bug https://github.com/gbif/checklistbank/issues/259
+      // else it should have thrown an error
+      name.match = {error: true};
+      callback();
     }
-    callback();
   }
 
   lookupNames = nameList => {
@@ -101,18 +109,19 @@ class MatchNames extends React.Component {
         if (err) {
           // TODO inform the user that not everything could be matched
           console.log(err);
-        } else {
-          if (that._isMount) {
-            let noMatches = [];
-            names.forEach(name => {
-              name.matchedName = name.match.formattedName || name.match.scientificName;
-              name.usageKey = name.match.usageKey;
-              name.key = name.name;
-              name.confidence = name.match.confidence;
-              if (!name.matchedName) noMatches.push(name);
-            });
-            that.setState({ matches: names, noMatches: noMatches, loading: false, progress: 0 });
-          }
+        }
+        if (that._isMount) {
+          let noMatches = [];
+          let errors = [];
+          names.forEach(name => {
+            name.matchedName = name.match.formattedName || name.match.scientificName;
+            name.usageKey = name.match.usageKey;
+            name.key = name.name;
+            name.confidence = name.match.confidence;
+            if (!name.matchedName) noMatches.push(name);
+            if (name.error) errors.push(name);
+          });
+          that.setState({ matches: names, noMatches: noMatches, loading: false, progress: 0, errors });
         }
       });
     }
@@ -120,12 +129,13 @@ class MatchNames extends React.Component {
 
   render() {
     const { setMatchedNames } = this.props;
-    const {progress} = this.state;
+    const { progress, errors } = this.state;
     return (
       <div>
         {this.state.loading && <MatchProgress total={this.props.names.length} matched={progress} />}
         <Table columns={columns} dataSource={this.state.matches} loading={this.state.loading} />
         {this.state.noMatches && this.state.noMatches.length > 0 ? <Alert message={`${this.state.noMatches.length} results without a match - no data will be shown for this name`} type="error" /> : undefined}
+        {this.state.noMatches && this.state.errors.length > 0 ? <Alert message={`${this.state.errors.length} api requests failed - these requests will count as no match`} type="error" /> : undefined}
         <Button type="primary" disabled={this.state.loading} loading={this.state.loading} onClick={() => {
           setMatchedNames(this.state.matches);
           this.props.history.push('/explore')
